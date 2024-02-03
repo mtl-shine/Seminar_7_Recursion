0f8e0b1b853dc3ae3ec3c667b95fbe22108379e8
Tatiana Nikitina <e.mtl.shine@gmail.com>
2024-02-03 16:17:36 +0100

    Ex_3 is done

--- Ex_3/Program.cs
+++ Ex_3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+using System.Xml.Serialization;
+
+int[] CreateRandomArray(int length)
+{
+    int[] randomArray = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < randomArray.Length; i++)
+    {
+        randomArray[i] = rnd.Next(100);
+    }
+    return randomArray;
+}
+Console.Write("Please, enter array length: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateRandomArray(length);
+
+int lastIndex = array.Length - 1;
+void PrintReversedArray(int[] array, int i)
+{
+    if (i == 0)
+    {
+        Console.Write(array[i] + " - reversed array");
+        return;
+    }
+    Console.Write(array[i] + ", ");
+    PrintReversedArray(array, i-1);
+}
+
+Console.WriteLine($"Origin array: {string.Join(", ", array)}");
+PrintReversedArray(array, lastIndex);
