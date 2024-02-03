e67121fb09e06607924acf3270aec7a821355a3f
Tatiana Nikitina <e.mtl.shine@gmail.com>
2024-02-03 16:02:25 +0100

    Ex_1 is done

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+Console.Write("Please, enter the number from which you want to start counting. M = ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter the number you want to count down to. N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+ShowNumbers(M,N);
+
+void ShowNumbers(int M, int N)
+{
+    if (M > N)
+    {
+        Console.Write("Conditions are not met. N must be greater than or equal to M");
+        return;
+    }
+    if (M == N)
+    {
+        Console.Write(N);
+        return;
+    }
+    Console.Write(M + ", ");
+    ShowNumbers(M + 1, N);
+}
